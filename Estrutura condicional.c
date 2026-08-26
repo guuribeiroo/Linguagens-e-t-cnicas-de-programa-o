c92f4e56060832c15f59fe127564b99f636d8176
Gustavo Ribeiro <gustavoslr2008@gmail.com>
2026-08-26 11:18:09 -0300

    Add files via upload

---
+++ Estrutura condicional.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*ESTRUTURA CONDICIONAL*/
+
+int main(int argc, char *argv[]) {
+	int a,b,c,r;
+	
+	
+	printf("Entre com os valores para serem comparados: "),
+	scanf("%d %d %d",&a,&b,&c);
+	
+	if(a > b){
+		r = a;
+	}else{
+		r = b;
+	}
+	if(c > r){
+		r = c;
+	}
+	printf("%d eh o maior!",r);
+	
+	
+	int n;
+	 
+	
+	printf("Insira um numero: ");
+	scanf("%d",&n);
+	
+	if(n % 2 == 0){
+		printf("O %d eh par",n);
+	}else{
+		printf("O %d eh impar",n);
+	}
+return 0;	
+}
+	
