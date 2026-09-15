f0ff3e1e6359fc861cfb9cd047b94fa6437e817d
Gustavo Ribeiro <gustavoslr2008@gmail.com>
2026-09-15 15:39:53 -0300

    Add initial structure for Simulado.c

---
+++ Simulado.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+ /* int a,b,c,d,aux1,aux2;
+  printf("Insira o valor de A B C D: ");
+  scanf("%d %d %d %d",&a,&b,&c,&d);
+  aux1 = a;
+  aux2 = c;
+  a = b;
+  b = d;
+  c = aux1;
+  d = aux3;
+
+  printf("%d %d %d %d",a,b,c,d);*/
+
+  
+
+  return 0;
+}
