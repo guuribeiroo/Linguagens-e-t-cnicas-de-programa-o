4b7038e318fa3d71f88baa1235b5b86c71e4ecad
Gustavo Ribeiro <gustavoslr2008@gmail.com>
2026-09-15 17:14:02 -0300

    Implement VPA and PVP calculations in Simulado.c
    Added functions to calculate VPA and PVP based on user input.

--- Simulado.c
+++ Simulado.c
@@ -2,6 +2,61 @@
 #include <stdlib.h>
 
 
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float calc_vpa(float valor_patrimonial, float quantidade_de_acoes){
+    return (valor_patrimonial / quantidade_de_acoes);
+}
+float calc_pvp(float preco_acoes, float vpa){
+    return (preco_acoes/vpa);
+}
+   
+int main(){
+  float valor_patrimonial,acoes_totais,preco_acoes,quantidade_de_acoes,pvp,vpa;
+  printf("Insira o valor partimonial: ");
+  scanf("%f",&valor_patrimonial);
+  printf("Insira a quantidade de ações disponiveis: ");
+  scanf("%f",&acoes_totais);
+  printf("Insira o preço atual da ação: ");
+  scanf("%f",&preco_acoes);
+  printf("=======Classificação do P/VP=======\n");
+
+vpa = calc_vpa(valor_patrimonial, quantidade_de_acoes);
+pvp = calc_pvp(preco_acoes, vpa);
+
+  if(pvp < 0){
+    printf("Péssima");
+  }
+  else if(pvp >= 0 && pvp < 0.8){
+    printf("Ótima");
+  }
+  else if(pvp >= 0.8 && pvp <= 1.2){
+    printf("Indiferente");
+  }
+  else if(pvp > 1.2 && pvp <= 2.0){
+    printf("Boa");
+  }
+  else if(pvp > 2.0){
+    printf("Ruim");
+  }
+
+  printf("\nVPA - %.2f\nPVP -%.2f",vpa,pvp);
+  return 0;
+
+
+
+
+}
+
+
+
+
+
+
 int main(){
  /* int a,b,c,d,aux1,aux2,aux3,aux4;
   printf("Insira o valor de A B C D: ");
