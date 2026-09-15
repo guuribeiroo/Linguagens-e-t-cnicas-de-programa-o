4d4a09711d09cb6a9c6e2e0e99e6463c2ba4b463
Gustavo Ribeiro <gustavoslr2008@gmail.com>
2026-09-15 15:53:22 -0300

    Refactor variable swapping in Simulado.c
    Updated variable declarations and modified the swapping logic for variables.

--- Simulado.c
+++ Simulado.c
@@ -3,17 +3,21 @@
 
 
 int main(){
- /* int a,b,c,d,aux1,aux2;
+ /* int a,b,c,d,aux1,aux2,aux3,aux4;
   printf("Insira o valor de A B C D: ");
   scanf("%d %d %d %d",&a,&b,&c,&d);
   aux1 = a;
-  aux2 = c;
-  a = b;
-  b = d;
-  c = aux1;
-  d = aux3;
+  aux2 = b;
+  aux3 = c;
+  aux4 = d;
+  a = c;
+  b = aux1;
+  c = d;
+  d = aux2;
 
-  printf("%d %d %d %d",a,b,c,d);*/
+  printf("%d %d %d %d",a,b,c,d);
+
+*/
 
   
 
